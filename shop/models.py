dc0528b5406f02012f9db58dedfdef5f7364ec70
zs <samadi83@gmail.com>
2021-03-11 04:49:33 -0800

    Income in model

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 # Create your models here.
 
 class Expense(models.Model):
@@ -7,3 +9,16 @@ class Expense(models.Model):
     date = models.DateField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __unicode__(self):
+        return self.text
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __unicode__(self):
+        return "{}{}".format(self.date, self.amount)
